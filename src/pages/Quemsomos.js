6445c55317050614bc91fb61bdf105a7104ab85e
Daniel de Souza Alves <53505046817@>
2023-11-06 11:29:13 -0300

    Modificando o texto do topo do Quem Somos

--- src/pages/Quemsomos.js
+++ src/pages/Quemsomos.js
@@ -7,15 +7,19 @@ const DivImage = styled.div`
     background-position: top;
     height: calc(25vh + 20vw);
     margin-top: 50px; 
+    padding: calc(1vh + 6vw);
 
-        
+    h1 {
+        font-size: 6vw;
+        background: red;
+    }
+    
 `
 
 function Quemsomos(){
     return(
         <DivImage>
-            <h1>uva</h1><h1>uva</h1><h1>uva</h1><h1>uva</h1>
-
+            <h1 class="h-full text-center text-white font-bold">CONHEÇA QUEM SOMOS E A HISTÓRIA DA TECPLAN</h1>
         </DivImage>
         
     )
